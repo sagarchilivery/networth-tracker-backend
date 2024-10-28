f17e2508ab7d1e73ee9c6d7d43a070c787bd9c3a
Sagar <chiliverysagar@gmail.com>
2024-10-29 03:55:51 +0530

    Updates

--- config.js
+++ config.js
@@ -6,6 +6,6 @@ dotenv.config();
 
 export const ENCRYPTION_KEY =
   process.env.ENCRYPTION_KEY || "12345678901234567890123456789012";
-export const sequelize = new Sequelize(process.env.DB_URI);
+export const sequelize = new Sequelize(process.env.DB_URI || "");
 export const PORT = process.env.PORT || 1337;
 export const JWT_SECRET = process.env.JWT_SECRET || "Luckiest Person on Earth";
